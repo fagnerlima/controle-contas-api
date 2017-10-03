e6fc70b4d9b3a3b5ac023c034cb0a1f30d1ba580
Fagner Lima <contato@fagnerlima.pro.br>
2017-10-02 21:39:11 -0300

    Primeiro commit - start-spring

---
+++ src/main/java/br/pro/fagnerlima/controlecontas/api/ControleContasApiApplication.java
@@ -0,0 +1,12 @@
+package br.pro.fagnerlima.controlecontas.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleContasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleContasApiApplication.class, args);
+	}
+}
